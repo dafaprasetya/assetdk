8f36a4944a5a5cd8c74727fed25dc7def791655c
prstya <prstya@WHOAMI.suckmy.dck>
2024-12-07 17:43:51 +0700

    update report dan fix ttd

--- resources/views/admin/IT/user.blade.php
+++ resources/views/admin/IT/user.blade.php
@@ -112,99 +112,102 @@
         </div>
     </div>
 </div>
-<table class="table table-bordered mt-3">
-<thead>
-    <tr>
-        <th>NIK</th>
-        <th>Nama</th>
-        <th>Email</th>
-        <th>Divisi</th>
-        <th>Action</th>
-    </tr>
-</thead>
-<tbody>
-    @foreach ($user as $user)
-    <tr>
-        <td>{{ $user->nik }}</td>
-        <td>{{ $user->nama }}</td>
-        <td>{{ $user->email }}</td>
-        <td>{{ $user->divisi }}</td>
-        <td>
-            @if (Auth::user()->nik == $user->nik)
-                Current User
-            @else
-                
-            <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#{{ Str::replace(array(' ','+'),'_',$user->nama) }}">Hapus</a>
-            <a class="btn btn-warning" href="#" data-toggle="modal" data-target="#{{ Str::replace(array(' ','+'),'_',$user->nama) }}{{ $user->id }}">Edit</a>
-            @endif
-            
-        </td>
-    </tr>
-    <div class="modal fade" id="{{ Str::replace(array(' ','+'),'_',$user->nama) }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Yakin ingin menghapus item ini?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Apakah anda ingin menghapus {{ $user->nama }} ini?, <b>semua data yang memiliki divisi "{{ $user->nama }}" akan terhapus semua secara otomatis setelah anda menekan tombol hapus</b></div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+<div class="table-responsive">
 
-                    <a class="btn btn-danger" href="#"
-                    onclick="event.preventDefault();
-                        document.getElementById('hapusform{{ $user->nik }}').submit();">
-                        {{ __('Hapus') }}
-                    </a>
-                    <form id="hapusform{{ $user->nik }}" action="{{ route('hapususer',$user->nik) }}" method="POST" class="d-none">
-                        @csrf
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="modal fade" id="{{ Str::replace(array(' ','+'),'_',$user->nama) }}{{ $user->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-    aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Item</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
+    <table class="table table-bordered mt-3">
+    <thead>
+        <tr>
+            <th>NIK</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Divisi</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($user as $user)
+        <tr>
+            <td>{{ $user->nik }}</td>
+            <td>{{ $user->nama }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->divisi }}</td>
+            <td>
+                @if (Auth::user()->nik == $user->nik)
+                    Current User
+                @else
                     
-                    <form id="editform{{ $user->nik }}" action="{{ route('edituser',$user->nik) }}" method="POST">
-                        @csrf
-                        <div class="form-group">
-                            <label for="nik">NIK : </label>
-                            <input id="nik" type="text" class="form-control" name="nik" value="{{ $user->nik }}">
-                            <label for="nama">Nama : </label>
-                            <input id="nama" type="text" class="form-control" name="nama" value="{{ $user->nama }}">
-                            <label for="email">Email : </label>
-                            <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}">
-                            <label for="nama">Password baru : </label>
-                            <input id="password" type="email" class="form-control" name="password" placeholder="masukan password baru">
-                        </div>
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-
-                    <a class="btn btn-warning" href="#"
-                    onclick="event.preventDefault();
-                        document.getElementById('editform{{ $user->nik }}').submit();">
-                        {{ __('Edit') }}
-                    </a>
+                <a class="btn btn-danger" href="#" data-toggle="modal" data-target="#{{ Str::replace(array(' ','+'),'_',$user->nama) }}">Hapus</a>
+                <a class="btn btn-warning" href="#" data-toggle="modal" data-target="#{{ Str::replace(array(' ','+'),'_',$user->nama) }}{{ $user->id }}">Edit</a>
+                @endif
+                
+            </td>
+        </tr>
+        <div class="modal fade" id="{{ Str::replace(array(' ','+'),'_',$user->nama) }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Yakin ingin menghapus item ini?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Apakah anda ingin menghapus {{ $user->nama }} ini?, <b>semua data yang memiliki divisi "{{ $user->nama }}" akan terhapus semua secara otomatis setelah anda menekan tombol hapus</b></div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+    
+                        <a class="btn btn-danger" href="#"
+                        onclick="event.preventDefault();
+                            document.getElementById('hapusform{{ $user->nik }}').submit();">
+                            {{ __('Hapus') }}
+                        </a>
+                        <form id="hapusform{{ $user->nik }}" action="{{ route('hapususer',$user->nik) }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    @endforeach
-</tbody>
-</table>
+        <div class="modal fade" id="{{ Str::replace(array(' ','+'),'_',$user->nama) }}{{ $user->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Edit Item</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        
+                        <form id="editform{{ $user->nik }}" action="{{ route('edituser',$user->nik) }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="nik">NIK : </label>
+                                <input id="nik" type="text" class="form-control" name="nik" value="{{ $user->nik }}">
+                                <label for="nama">Nama : </label>
+                                <input id="nama" type="text" class="form-control" name="nama" value="{{ $user->nama }}">
+                                <label for="email">Email : </label>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $user->email }}">
+                                <label for="nama">Password baru : </label>
+                                <input id="password" type="email" class="form-control" name="password" placeholder="masukan password baru">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+    
+                        <a class="btn btn-warning" href="#"
+                        onclick="event.preventDefault();
+                            document.getElementById('editform{{ $user->nik }}').submit();">
+                            {{ __('Edit') }}
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </tbody>
+    </table>
+</div>
 @endsection
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -210,7 +210,7 @@
             <form
                 class="d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" action="{{ route('buatlaporandkl') }}">
                 <div class="input-group">
-                    <input type="date" class="form-control bg-light border-0 small" placeholder="report" aria-label="report" aria-describedby="basic-addon2" id="datee" name="">
+                    <input type="date" class="form-control bg-light text-dark border-0 small" placeholder="report" aria-label="report" aria-describedby="basic-addon2" id="datee" name="">
                     <input type="text" class="form-control bg-light border-0 small" placeholder="report" aria-label="report" aria-describedby="basic-addon2" id="report" name="report" hidden>
                     <button type="submit" id="generate-report"  class="d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                         <i class="fas fa-download fa-sm text-white-50"></i>
